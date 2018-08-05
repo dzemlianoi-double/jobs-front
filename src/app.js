77f530112fea204a710ad246a4abb0a299d699fa
Dzemlianoi <dzemlianoi@gmail.com>
2018-08-05 13:41:57 +0300

    i18n-added

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import Header from './layout/header';
 import Main from './layout/main';
 import Footer from './layout/footer';
+import T from 'i18n-react';
+import translations from './translations/ru.json';
 
 export default class App extends Component {
+  componentDidMount() {
+    T.setTexts(translations);
+  }
+
   render () {
     return (
       <div>
+        <T.span text="hello"></T.span>
         <Header />
         <Main />
         <Footer />

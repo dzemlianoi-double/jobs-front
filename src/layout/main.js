329c823babb7817f6ea4fe5ece1c4b7af54fbc83
Denis Zemlianoy <dzemlianoi@gmail.com>
2018-08-07 21:46:01 +0300

    spinner added

--- src/layout/main.js
+++ src/layout/main.js
@@ -4,16 +4,16 @@ import AboutUs from './about-us';
 import Services from './services';
 import Portfolio from './portfolio';
 import { Route } from 'react-router-dom';
-import routes from '../config/internal_routes';
+import internal_routes from '../config/internal_routes';
 
 class Main extends Component {
   render() {
     return (
       <main>
-        <Route exact path={routes.home} component={Home} />
-        <Route exact path={routes.about_us} component={AboutUs}/>
-        <Route exact path={routes.services} component={Services}/>
-        <Route exact path={routes.contacts} component={Portfolio}/>
+        <Route exact path={internal_routes.home} component={Home} />
+        <Route exact path={internal_routes.about_us} component={AboutUs}/>
+        <Route exact path={internal_routes.services} component={Services}/>
+        <Route exact path={internal_routes.contacts} component={Portfolio}/>
       </main>
     );
   }
---
+++ src/layout/spinner.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div className="spinner-wrapper">
+        <div class="spinner-content">
+          <p>Загрузка ...</p>
+          <div className="loading">
+            <div className="l1"></div>
+            <div className="l2"></div>
+            <div className="l3"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

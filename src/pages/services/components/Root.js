88bfeccada3835533726bf73875eff80387984bf
keks13 <moaner48@gmail.com>
2018-08-18 20:12:49 +0300

    added service page

--- src/pages/services/components/Root.js
+++ src/pages/services/components/Root.js
@@ -3,7 +3,68 @@ import React, { Component } from 'react';
 export default class Services extends Component {
   render () {
     return (
-      <div>rederect on services</div>
+      <div className="services">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="container mt-25">
+              <p className="h1 fs-30 text-center">Services</p>
+              <p className="text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa cum sociis.</p>
+              <div className="service mb-25">
+                <div className="row">
+                  <div className="col-md-4 about-service">
+                    <div className="service-block">
+                      <p className="title-service fs-15">
+                        <b>Рабочая виза и трудоустройство в Польше</b>
+                      </p>
+                      <div className="text-center">
+                        <span className="price fs-25">3000</span>
+                      </div>
+                        <p className="description-service">Подбор вакансии. Рабочая виза. Трудоустройство. Дальнейшее сопровождение.</p>
+                      <div className="row">
+                        <div className="col-md-12 btn-use-service">
+                          <button>Use</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4 about-service">
+                    <div className="service-block">
+                      <p className="title-service fs-15">
+                        <b>Рабочая виза и трудоустройство в Польше</b>
+                      </p>
+                      <div className="text-center">
+                        <span className="price fs-25">3000</span>
+                      </div>
+                        <p className="description-service">Подбор вакансии. Рабочая виза. Трудоустройство. Дальнейшее сопровождение.</p>
+                      <div className="row">
+                        <div className="col-md-12 btn-use-service">
+                          <button>Use</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4 about-service">
+                    <div className="service-block">
+                      <p className="title-service fs-15">
+                        <b>Рабочая виза и трудоустройство в Польше</b>
+                      </p>
+                      <div className="text-center">
+                        <span className="price fs-25">3000</span>
+                      </div>
+                        <p className="description-service">Подбор вакансии. Рабочая виза. Трудоустройство. Дальнейшее сопровождение.</p>
+                      <div className="row">
+                        <div className="col-md-12 btn-use-service">
+                          <button>Use</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }

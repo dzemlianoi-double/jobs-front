b9a6a5eb2ff75a09d96394c6fb49663eae3bd3dd
Denis Zemlianoy <dzemlianoi@gmail.com>
2018-08-19 20:20:35 +0300

    autosuggest added

---
+++ src/pages/vacancies/components/search/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import SearchAutosuggestions from './SearchAutosuggestions';
+import T from '../../../store/translations';
+
+export default class Search extends Component {
+  render(){
+    return (
+      <div className='row'>
+        <div className='col-md-12 mu-vacancies-search'>
+          <div className="vacancy-label inline-block">
+            <T.span text="vacancies.vacancy" />
+            <SearchAutosuggestions {...this.props} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+

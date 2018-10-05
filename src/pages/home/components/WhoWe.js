2107711b08c7ed8af25952ff64ffbd81397b97f3
IRuban <moaner48@gmail.com>
2018-10-05 15:26:03 +0300

    updated info in who_we

--- src/pages/home/components/WhoWe.js
+++ src/pages/home/components/WhoWe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import T from '../../../store/translations';
-import office from '../../../assets/images/office.jpg';
 
 const WhoWe = () => {
   return(
@@ -14,32 +14,58 @@ const WhoWe = () => {
                 <div className='col-md-12'>
                   <div className='mu-title'>
                     <h2>{T.translate('who_we.caption_whowe')}</h2>
+                    <p className="we-title">{T.translate('who_we.our_headline')}</p>
                   </div>
                 </div>
               </div>
               <div className='row'>
                 <div className='col-md-6'>
+                  <div className='row'>
+                    <div className='col-md-12'>
+                      <div className="img"></div>
+                    </div>
+                  </div>
                   <div className='mu-about-left'>
-                    <img src={office} alt='office' />
+                    <ul>
+                      <li>
+                        <h3>{T.translate('who_we.we_provide')}</h3>
+                        <T.p text="who_we.provide_values_1" />
+                        <T.p text="who_we.provide_values_2" />
+                      </li>
+                    </ul>
                   </div>
                 </div>
                 <div className='col-md-6'>
                   <div className='mu-about-right'>
                     <ul>
                       <li>
+                        <h3>{T.translate('who_we.our_benefits')}</h3>
+                        <T.p text="who_we.benefits_values_1" />
+                        <T.p text="who_we.benefits_values_2" />
+                        <T.p text="who_we.benefits_values_3" />
+                        <T.p text="who_we.benefits_values_4" />
+                      </li>
+                      <li>
                         <h3>{T.translate('who_we.our_mission')}</h3>
-                        <T.p text="who_we.text_mission" />
+                        <T.p text="who_we.mission_value" />
                       </li>
                       <li>
                         <h3>{T.translate('who_we.our_goal')}</h3>
-                        <T.p text="who_we.text_goal" />
+                        <T.p text="who_we.goal_values" />
                       </li>
                       <li>
-                        <h3>{T.translate('who_we.our_values')}</h3>
-                        <T.p text="who_we.text_values_1" />
-                        <T.p text="who_we.text_values_2" />
-                        <T.p text="who_we.text_values_3" />
-                        <T.p text="who_we.text_values_4" />
+                        <h3>{T.translate('who_we.we_offer')}</h3>
+                        <T.p text="who_we.offer_value" />
+                        <T.span text="who_we.offer_package_value_1" />
+                        <T.span text="who_we.more" />
+                        <Link className="display-inline" to="/services"> {T.translate('who_we.packege_profi')}</Link>
+                        <p>
+                          <T.span text="who_we.offer_package_value_2" />
+                          <Link className="display-inline" to="/services"> {T.translate('who_we.packege_biometric_passport')}</Link>
+                        </p>
+                        <T.span text="who_we.offer_package_value_3" />
+                        <T.span text="who_we.more" />
+                        <Link className="display-inline" to="/services"> {T.translate('who_we.packege_standart')}</Link>
                       </li>
                     </ul>
                   </div>
